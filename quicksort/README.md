d01eb8297c7ddf29e09f9a30162df4481288e37f
Hironha <rafaelhirokato@gmail.com>
2024-06-10 22:24:54 -0300

    improve partition impl

--- quicksort/README.md
+++ quicksort/README.md
@@ -2,7 +2,7 @@
 
 Quicksort is one of the most efficient sort algorithms that uses a divide and conquer strategy to improve the average sorting speed. It also does not utilize auxiliary space, except for the stack frames used in recursion, and it's ordinary algorithm is quite easy to implement, although it can get some edge cases where it gets off by one if the partition is not done correctly.
 
-The most important part of this algorithm is the `partition`. The partition is responsible for sorting parts of the array. The partition function selects a `pivot`, generally the last element of the slice, and makes sure every element ahead of the pivot is bigger, meaning the pivot it's in the right place.
+The most important part of this algorithm is the `partition`. The partition is responsible for sorting parts of the array. The partition function selects a `pivot`, generally the last element of the slice, and makes sure every element before of the pivot is smaller, meaning the pivot it's in the right place.
 
 - Time Complexity: O(n²) but on average O(n \* log(n))
 - Space Complexity: O(1) ignoring stack frames used by recursion
--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn quicksort<T: PartialOrd>(slice: &mut [T]) {
-    if slice.is_empty() {
+    if slice.len() <= 1 {
         return;
     }
 
@@ -15,24 +15,24 @@ fn partition<T: PartialOrd>(slice: &mut [T]) -> usize {
         return 0;
     }
 
-    let mut i = 0usize;
-    // use last element as pivot
-    let mut pivot = slice.len() - 1;
+    let pivot_idx = slice.len() - 1;
+    // `i` represents the position which pivot should be
+    let mut i = 0;
 
-    while i < pivot {
-        // if current value is bigger than pivot, move pivot one element back
-        // and swap current value to the previous position of pivot
-        if slice[i] >= slice[pivot] {
-            slice.swap(i, pivot - 1);
-            slice.swap(pivot - 1, pivot);
-            pivot -= 1;
-        } else {
+    for j in 0..pivot_idx {
+        if slice[j] <= slice[pivot_idx] {
+            // move values smaller than pivot to left of pivot
+            slice.swap(i, j);
+            // increment the position pivot should be, since `i` has a value
+            // smaller than pivot after the swap
             i += 1;
         }
     }
 
-    // return pivot as partition
-    pivot
+    // move pivot to the correct position
+    slice.swap(i, pivot_idx);
+
+    i
 }
 
 #[cfg(test)]
@@ -59,13 +59,7 @@ mod tests {
 
     #[test]
     fn partially_sorted() {
-        let mut input = [
-            75, 78, 92, 1, 95, 44, 53, 43, 37, 91, 71, 18, 44, 96, 86, 6, 20, 64, 29, 24, 88, 56,
-            19, 0, 32, 47, 14, 9, 86, 39, 15, 57, 71, 22, 25, 45, 71, 59, 80, 42, 6, 48, 26, 19,
-            99, 6, 89, 19, 54, 18, 0, 4, 55, 0, 84, 57, 90, 67, 35, 4, 61, 22, 95, 62, 74, 55, 79,
-            64, 71, 10, 53, 74, 16, 67, 54, 2, 78, 71, 99, 18, 92, 17, 95, 2, 40, 27, 28, 70, 2,
-            71, 17, 94, 56, 71, 76, 66, 43, 52, 63, 71,
-        ];
+        let mut input = [12, 4, 3, 2, 1, 5, 4, 7, 8, 9];
         quicksort(&mut input);
 
         assert!(is_sorted(&input));
